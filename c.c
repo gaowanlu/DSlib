308495db854ab82c4495eb8bac8dcd14d8fb2163
gaowanlu <2209120827@qq.com>
2020-09-21 22:08:48 +0800

    new

---
+++ c.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include"type.h"
+struct _SeqList* _SeqList_init(struct _SeqList*List){
+	if(!List||!(List->size)){
+		return NULL;
+	}
+	List->data=(union _SeqList_node*)malloc(sizeof(union _SeqList_node)*(List->size));
+	if(!(List->data)){
+		return NULL;
+	}
+	return List;
+};
+int main(void){
+	struct _SeqList List;
+	List.size=10;
+	_SeqList_init(&List);
+	int i=0;
+	for(i=0;i<10;i++){
+		(List.data+i)->data_int=i;
+	}
+	for(i=0;i<10;i++){
+		printf("%d\n",(List.data+i)->data_int);
+	}
+	return 0;
+}
---
+++ type.h
@@ -0,0 +1,18 @@
+#ifndef __TYPE_H__
+#define __TYPE_H__
+union _SeqList_node{
+	int data_int;
+	float data_float;
+	char data_char;
+	double data_double;
+};
+struct _SeqList{
+	size_t size;
+	union _SeqList_node*data; 
+};
+#endif
+
+
+
+
+
