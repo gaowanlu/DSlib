8e03a5fcc729229d83f867894de839631e9d937b
gaowanlu <2209120827@qq.com>
2020-09-22 10:01:56 +0800

    c.c function remove type.h

--- c.c
+++ c.c
@@ -1,36 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include"type.h"
-struct _SeqList** _SeqList_getLength(struct _SeqList**List){
-	if(!*List||!((*List)->size)){
-		return NULL;
-	}
-	(*List)->data=(union _SeqList_node*)malloc(sizeof(union _SeqList_node)*((*List)->size));
-	if(!((*List)->data)){
-		return NULL;
-	}
-	return List;
-};
-struct _SeqList** _SeqList_clear(struct _SeqList**List){
-	if(!List){
-		return NULL;
-	}
-	//to free
-	free((*List)->data);
-	(*List)->data=NULL;
-	(*List)->size=0;
-	return List;
-}
-struct _SeqList* _SeqList_init(struct _SeqList**List){
-	//对二维指针申请内存，并将结构体内部的函数指针指向相应的函数
-	*List=(struct _SeqList*)malloc(sizeof(struct _SeqList)*1);
-	if(!*List){
-		return 0;
-	}
-	(*List)->getLength=_SeqList_getLength;
-	(*List)->clear=_SeqList_clear;
-	return *List;
-}
 int main(void){
 	struct _SeqList *List;
 	_SeqList_init(&List);
@@ -38,12 +8,23 @@ int main(void){
 	List->getLength(&List);
 	int i=0;
 	for(i=0;i<10;i++){
-		(List->data+i)->data_int=i;
+		(List->data+i)->data_int=i+999;
 	}
 	for(i=0;i<10;i++){
 		printf("%d\n",(List->data+i)->data_int);
 	}
 	List->clear(&List);
 	return 0;
-
+/*运行结果
+999
+1000
+1001
+1002
+1003
+1004
+1005
+1006
+1007
+1008
+*/
 }
--- type.h
+++ type.h
@@ -1,5 +1,11 @@
+
 #ifndef __TYPE_H__
 #define __TYPE_H__
+union _SeqList_node;
+struct _SeqList;
+struct _SeqList** getLength(struct _SeqList**List);
+struct _SeqList** clear(struct _SeqList**List);
+struct _SeqList** init(struct  _SeqList**List);
 union _SeqList_node{
 	int data_int;
 	float data_float;
@@ -12,6 +18,36 @@ struct _SeqList{
 	struct _SeqList** (*getLength)(struct _SeqList**List);
 	struct _SeqList** (*clear)(struct _SeqList**List);
 };
+struct _SeqList** _SeqList_getLength(struct _SeqList**List){
+	if(!*List||!((*List)->size)){
+		return NULL;
+	}
+	(*List)->data=(union _SeqList_node*)malloc(sizeof(union _SeqList_node)*((*List)->size));
+	if(!((*List)->data)){
+		return NULL;
+	}
+	return List;
+};
+struct _SeqList** _SeqList_clear(struct _SeqList**List){
+	if(!List){
+		return NULL;
+	}
+	//to free
+	free((*List)->data);
+	(*List)->data=NULL;
+	(*List)->size=0;
+	return List;
+}
+struct _SeqList* _SeqList_init(struct _SeqList**List){
+	//对二维指针申请内存，并将结构体内部的函数指针指向相应的函数
+	*List=(struct _SeqList*)malloc(sizeof(struct _SeqList)*1);
+	if(!*List){
+		return 0;
+	}
+	(*List)->getLength=_SeqList_getLength;
+	(*List)->clear=_SeqList_clear;
+	return *List;
+}
 #endif
 
 
