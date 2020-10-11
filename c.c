ccee3954bc31440804571319ebbd8ef11dbcc062
gaowanlu <2209120827@qq.com>
2020-10-11 13:16:26 +0800

    新增链表数据结构

--- c.c
+++ c.c
@@ -15,5 +15,33 @@ int main(void){
 	string2->set(&string2,"er");
 	printf("%ld\n",string->kmp(&string,&string2));
 	printf("%ld\n",string->bf(&string,&string2));
+
+
+
+	//进行链表测试
+	struct _LinkList*list;
+	_LinkList_init(&list);
+	struct _LinkList_node TEMP;
+	(TEMP.data).data_int=0;
+	for(int i=0;i<10;i++){
+		(TEMP.data).data_int=i;
+		//list->afterInster(&list,&TEMP);
+		list->beforeInster(&list,&TEMP);
+	}
+	//输出链表
+	struct _LinkList_node *loop=list->headNode->next;
+	while(loop){
+		printf("%d\n",(loop->data).data_int);
+		loop=loop->next;
+	}
+	//link to reverse
+	printf("reverse\n");
+	list->reverse(&list);
+	loop=list->headNode->next;
+	while(loop){
+		//printf("::::\n");
+		printf("%d\n",(loop->data).data_int);
+		loop=loop->next;
+	}
 	return 0;
 }
